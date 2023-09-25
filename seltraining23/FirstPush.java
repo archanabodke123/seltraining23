85fac251e4b86b2ced806f6ee21ddcac92e83593
archa <archa@LAPTOP-8SE00MKG>
2023-09-25 22:11:59 +0530

    My first push code

--- seltraining23/FirstPush.java
+++
@@ -1,7 +0,0 @@
-package seltraining23;
-
-public class FirstPush {
-public static void main(String[] args) {
-	System.out.println("My first push");
-}
-}
---
+++ src/seltraining23/FirstPush.java
@@ -0,0 +1,12 @@
+package seltraining23;
+
+public class FirstPush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My First Push");
+
+	}
+
+}
