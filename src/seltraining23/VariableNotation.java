67a2df5308cb7aef04d24c790d3c0efef4595cd0
archa <archa@LAPTOP-8SE00MKG>
2023-09-26 11:46:22 +0530

    Variable Notation code commit

---
+++ src/seltraining23/VariableNotation.java
@@ -0,0 +1,23 @@
+package seltraining23;
+
+public class VariableNotation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10; //this is correct
+
+		int int = 20; //this is incorrect as int is datatype
+
+		int goto = 30; //this is incorrect as goto is keyword
+
+		int default = 40; //this is incorrect as default is keyword
+
+		int class = 50; //this is incorrect as class is keyword
+
+		int j = 70; //this is correct
+
+
+	}
+
+}
